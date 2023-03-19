e167b04ea4614281c90c4f50031b1737088e3ea7
Isabel Encina <117072817+IzziEnso@users.noreply.github.com>
2023-03-19 23:56:17 +0100

    Add files via upload
    Práctica 4.4 de Estructuras Básicas

---
+++ Ejer4.4.4/Ejer4.4.4/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Ejer4._4._4
+{
+    class Program
+    {
+        static void Main(String[]Args)
+        {
+            Console.WriteLine("NÚMERO DE ALUMNOS");
+            double n = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+
+            while (i < n)
+            {
+
+                Console.WriteLine("NOMBRE");
+                String nombre = (Console.ReadLine());
+                double promedio;
+
+                Console.WriteLine("NOTA 1: ");
+                double nota1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("NOTA 2: ");
+                double nota2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("PROMEDIO: ");
+                promedio = (nota1 + nota2) / 2;
+                Console.WriteLine(promedio);
+
+                i++;
+            }
+        }
+    }
+}
+
